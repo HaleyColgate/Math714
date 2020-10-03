ff5a48cbba63bf55060f33b38606fadb888ed1c8
HaleyColgate <51422024+HaleyColgate@users.noreply.github.com>
2020-10-03 13:04:46 -0500

    Add files via upload

--- HW1.m
+++ HW1.m
@@ -1,7 +1,30 @@
-h = .005;
+h = .01;
 Z = GS(h, 10000);
 [X,Y] = meshgrid(0:h:1,0:h:1);
-surf(X, Y, Z)
+solution = Analytical(h);
+subplot(1,3, 1)
+surf(X, Y, solution)
+subplot(1,3,2)
+surf(X,Y,Z)
+subplot(1,3,3)
+error = abs(Z-solution);
+surf(X,Y,error)
+max(max(error))
+CalcError(1000000)
+
+function soltn = Analytical(h)
+    m = int32(1/h);
+    soltn = zeros(m+1);
+    xcount = 0;
+    for i = 0:h:1
+        xcount = xcount+1;
+        ycount = 0;
+        for j = 0:h:1
+            ycount = ycount+1;
+            soltn(ycount, xcount) = cos(2*pi*j)*(cosh(2*pi*i)-(cosh(2*pi)/sinh(2*pi))*sinh(2*pi*i));
+        end
+    end
+end
 
 function u = GS(h, maxiter)
     m = int32(1/h);
@@ -21,4 +44,18 @@ function u = GS(h, maxiter)
             u(m+1,n) = u(m,n);
         end
     end
+end
+
+function error = CalcError(maxiter)
+    evec = zeros(1, 8);
+    hvec = zeros(1, 8);
+    hval = [.05, .025, .02, .01, .005, .004, .0025, .002];
+    for counter = 1:8
+        hvec(counter) = hval(counter);
+        errorMtx = abs(GS(hval(counter),maxiter)-Analytical(hval(counter)));
+        evec(counter) = max(max(errorMtx))
+    end
+    hvec = log(hvec)
+    evec = log(evec)
+    plot(hvec,evec)
 end
